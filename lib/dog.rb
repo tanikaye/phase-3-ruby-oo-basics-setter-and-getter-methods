f2bcb43bc21b73ac5c5fa7f00b1e6c641b34718b
Tani Kaye <1tanikaye@gmail.com>
2021-11-23 11:53:01 -0500

    assignment completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+  def name=(param)
+    @name = param
+     end
 
+    def name
+      @name
+     end
+
+     def breed=(param)
+      @breed = param
+       end
+
+      def breed
+        @breed
+       end
 end
