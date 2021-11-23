881fb5e800b00d43e97aeb9d7e3753ce2b113da7
Tani Kaye <1tanikaye@gmail.com>
2021-11-23 11:52:52 -0500

    commit tree d9a04f985a8af9eb26cf4dea07ecdfb60cdc27db

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
+  def name=(param)
+    @name = param
+     end
 
+    def name
+      @name
+     end
+
+     def job=(param)
+      @job = param
+       end
+
+      def job
+        @job
+       end
 end
